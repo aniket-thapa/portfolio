fb713897ec5a1236eb826c41a1c023289a25a45c
Aniket Thapa <aniketthapa04@gmail.com>
2024-12-19 22:59:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 document
   .querySelector(".hamburger-menu")
   .addEventListener("click", function () {
-    document.querySelector(".side-nav").style.width = "100%";
+    document.querySelector(".side-nav").style.width = "100vw";
+    document.querySelector(".side-nav").style.height = "100vh";
     document.body.style.overflow = "hidden";
   });
 
